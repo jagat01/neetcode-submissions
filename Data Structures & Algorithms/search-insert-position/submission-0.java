aa0d2475f74a9473d350e9c5e56f0e7df083225c
Jagat Raj <jraj77700@gmail.com>
2026-06-20 22:33:25 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int boundryIndex=-1;
+        if(nums[r]<target){
+            return r+1;
+        }
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>=target){
+                boundryIndex=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return boundryIndex;
+    }
+}
